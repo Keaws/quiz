76e8617a3099e653d94fa993decdf2beb4a7bec1
Vitalii Kalchuk <v.sweak@gmail.com>
2017-01-29 16:54:21 +0200

    Add procfile, update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
 app.use(express.static(__dirname + '/public'));
